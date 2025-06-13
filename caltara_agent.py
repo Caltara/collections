f97a8575c1d5074f5c57f2e259981421b08bf6b3
Caltara <caltaraai@gmail.com>
2025-06-13 19:06:35 -0400

    Update caltara_agent.py

--- caltara_agent.py
+++ caltara_agent.py
@@ -8,7 +8,7 @@ TWILIO_PHONE = st.secrets["TWILIO_PHONE_NUMBER"]
 client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
 def send_sms(to, name, amount, due_date):
-    message = f"Hi {name}, this is a friendly reminder from Caltara. You have an unpaid invoice of ${amount} due on {due_date}. Please reply or call to arrange payment."
+    message = f"Hi {name}, this is a friendly reminder from Furry Friends. You have an unpaid invoice of ${amount} due on {due_date}. Please reply or call to arrange payment."
     return client.messages.create(
         body=message,
         from_=TWILIO_PHONE,
