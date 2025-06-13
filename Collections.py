0c8e6a37fc0f4d79e9db57c7b2dd75442cf8f54c
Caltara <caltaraai@gmail.com>
2025-06-12 20:53:43 -0400

    Create Collections.py

---
+++ Collections.py
@@ -0,0 +1,21 @@
+from twilio.rest import Client
+from twilio_config import TWILIO_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE
+
+client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+def send_sms(to, name, amount, due_date):
+    message = f"Hi {name}, this is a friendly reminder from Caltara. You have an unpaid invoice of ${amount} due on {due_date}. Please reply or call to arrange payment."
+    return client.messages.create(
+        body=message,
+        from_=TWILIO_PHONE,
+        to=to
+    )
+
+def send_voice(to, name, amount, due_date):
+    twiml = f"<Response><Say>Hi {name}, this is a payment reminder from Caltara. You have an outstanding invoice of {amount} dollars, due on {due_date}. Please press 1 to talk to a representative or visit your invoice link.</Say></Response>"
+    call = client.calls.create(
+        twiml=twiml,
+        to=to,
+        from_=TWILIO_PHONE
+    )
+    return call
